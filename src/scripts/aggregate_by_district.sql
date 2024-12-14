cffebb2f1d7f7826e4e1de30aa2c5fa3f6840e97
Ian Grant <ian.conway.grant@gmail.com>
2024-12-13 22:38:56 -0500

    removed duplicated section of code

--- src/scripts/aggregate_by_district.sql
+++ src/scripts/aggregate_by_district.sql
@@ -25,22 +25,6 @@ GROUP BY cd.geom;
 
 -- Update the types to INT32 for the aggregation columns
 -- (DuckDB likes to make huge ints)
--- Create temporary table with district aggregations
-CREATE TEMPORARY TABLE district_counts AS
-SELECT 
-    cd.geom,
-    COUNT(*) AS total_reports,
-    SUM(CASE WHEN dl.no_electricity THEN 1 ELSE 0 END) AS power_outages,
-    SUM(CASE WHEN dl.basement_flooded THEN 1 ELSE 0 END) AS flooded_basements,
-    SUM(CASE WHEN dl.roof_damaged THEN 1 ELSE 0 END) AS roof_damage,
-    SUM(CASE WHEN dl.insurance THEN 1 ELSE 0 END) AS with_insurance
-FROM damage_locations AS dl
-JOIN community_districts AS cd
-ON ST_Within(dl.geom, cd.geom)
-GROUP BY cd.geom;
-
--- Update the types to INT32 for the aggregation columns
--- (DuckDB likes to make huge ints)
 -- Surely there is a better way to do this
 ALTER TABLE district_counts ALTER total_reports SET DATA TYPE INT32;
 ALTER TABLE district_counts ALTER power_outages SET DATA TYPE INT32;
